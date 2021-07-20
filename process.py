e5f7f7391b63b6ae4da75b288006cffa6eae7a36
Sarah Patanroi <sarahpatanroi@gmail.com>
2021-07-20 21:24:44 +0000

    Added function description

--- process.py
+++ process.py
@@ -1,12 +1,12 @@
 log_file = open("./um-server-01.txt")
 
-
 def sales_reports(log_file):
+    '''Function pulls Monday delivery data from server file'''
+
     for line in log_file:
         line = line.rstrip()
         day = line[0:3]
         if day == "Mon":
             print(line)
-
 
 sales_reports(log_file)
